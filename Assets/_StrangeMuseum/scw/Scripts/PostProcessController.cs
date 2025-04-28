13fc21ab00771be7563b94be8a1c719c5fd6db1c
seocxwoo <seocxwoo@gmail.com>
2025-04-29 01:29:18 +0900

    add

--- Assets/_StrangeMuseum/scw/Scripts/PostProcessController.cs
+++ Assets/_StrangeMuseum/scw/Scripts/PostProcessController.cs
@@ -1,15 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class PostProcessController : MonoBehaviour
 {
+    public Transform playerTr;
+    public Transform statueTr;
+    private bool isNearby = false;
+
     public Volume globalVolume;
 
     private ChromaticAberration chromaticAberration;
     private LensDistortion lensDistortion;
 
     private ColorAdjustments colorAdjustments;
+    private Vignette vignette;
 
     private float timer;
     private float weirdInterval = 10f; // 몇 초 간격으로 기괴해질지
@@ -33,32 +39,82 @@ public class PostProcessController : MonoBehaviour
         {
             colorAdjustments.active = true;
         }
+
+        if (globalVolume.profile.TryGet(out vignette))
+        {
+            vignette.active = true;
+        }
+
+        StartCoroutine("CheckDistanceStatue");
+    }
+
+    IEnumerator CheckDistanceStatue()
+    {
+        while (true)
+        {
+            if (Vector3.Distance(playerTr.position, statueTr.position) < 10.0f)
+            {
+                isNearby = true;
+            }
+            else
+            {
+                isNearby = false;
+            }
+            yield return new WaitForSeconds(3.0f);
+        }
     }
 
     void Update()
     {
-        // 크로마틱 어베레이션 세기 조절 (0~1)
-        if (chromaticAberration != null)
+        if(isNearby)
         {
-            //chromaticAberration.intensity.value = Mathf.PingPong(Time.time * 0.2f, 1f);
+            // 크로마틱 어베레이션 세기 조절 (0~1)
+            if (chromaticAberration != null)
+            {
+                //chromaticAberration.intensity.value = Mathf.PingPong(Time.time * 0.2f, 1f);
+                chromaticAberration.intensity.value = 1.0f;
+            }
+
+            // 렌즈 왜곡 효과 조절 (-1~1)
+            if (lensDistortion != null)
+            {
+                lensDistortion.intensity.value = Mathf.Sin(Time.time) * 0.1f;
+            }
+
+            if (vignette != null)
+            {
+                vignette.intensity.value = 0.4f;
+            }
+
+            timer += Time.deltaTime;
+
+            if (!isWeird && timer >= weirdInterval)
+            {
+                StartWeirdEffect();
+            }
+
+            if (isWeird && timer >= weirdDuration)
+            {
+                EndWeirdEffect();
+            }
         }
-
-        // 렌즈 왜곡 효과 조절 (-1~1)
-        if (lensDistortion != null)
+        else
         {
-            lensDistortion.intensity.value = Mathf.Sin(Time.time) * 0.1f;
-        }
+            if (chromaticAberration != null)
+            {
+                chromaticAberration.intensity.value = 0f;
+            }
 
-        timer += Time.deltaTime;
+            // 렌즈 왜곡 효과 조절 (-1~1)
+            if (lensDistortion != null)
+            {
+                lensDistortion.intensity.value = 0f;
+            }
 
-        if (!isWeird && timer >= weirdInterval)
-        {
-            StartWeirdEffect();
-        }
-
-        if (isWeird && timer >= weirdDuration)
-        {
-            EndWeirdEffect();
+            if (vignette != null)
+            {
+                vignette.intensity.value = 0f;
+            }
         }
     }
 
