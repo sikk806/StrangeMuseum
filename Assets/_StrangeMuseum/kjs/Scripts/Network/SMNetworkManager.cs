d076cafa6b9feea9ada87d4ad68a9ff1b2953e95
sikk806 <sikk806@gmail.com>
2025-05-02 10:03:19 +0900

    test

--- Assets/_StrangeMuseum/kjs/Scripts/Network/SMNetworkManager.cs
+++ Assets/_StrangeMuseum/kjs/Scripts/Network/SMNetworkManager.cs
@@ -6,16 +6,23 @@ using Steamworks;
 
 public class SMNetworkManager : NetworkManager
 {
-    [SerializeField] private List<PlayerLobbyController> GamePlayerPrefab;
+    [SerializeField] private PlayerLobbyController GamePlayerPrefab;
+    [SerializeField] private GameObject securityPrefab;
+    [SerializeField] private GameObject statuePrefab;
 
     public List<PlayerLobbyController> GamePlayers { get; } = new List<PlayerLobbyController>(); // Info of Players
 
+    protected void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
-        if (SceneManager.GetActiveScene().name == "NetworkTest")
+        if (SceneManager.GetActiveScene().name == "Lobby")
         {
-             PlayerLobbyController GamePlayerInstance = Instantiate(GamePlayerPrefab[0]);
-             GamePlayerInstance.transform.position = Vector3.zero;
+            PlayerLobbyController GamePlayerInstance = Instantiate(GamePlayerPrefab);
+            GamePlayerInstance.transform.position = Vector3.zero;
 
             GamePlayerInstance.ConnectionID = conn.connectionId;
             GamePlayerInstance.PlayerIdNumber = GamePlayers.Count + 1;
@@ -39,4 +46,36 @@ public class SMNetworkManager : NetworkManager
     {
         ServerChangeScene(sceneName);
     }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "NetworkTest")
+        {
+            foreach(var player in GamePlayers)
+            {
+                NetworkServer.DestroyPlayerForConnection(player.connectionToClient);
+                GameObject playerObj = null;
+
+                int randomPrefab = Random.Range(0, 1);
+                if(randomPrefab == 0)
+                {
+                    playerObj = Instantiate(securityPrefab);
+                }
+                else if(randomPrefab == 1)
+                {
+                    playerObj = Instantiate(statuePrefab);
+                }
+                NetworkServer.AddPlayerForConnection(player.connectionToClient, playerObj);
+
+                PlayerController playerController = playerObj.GetComponent<PlayerController>();
+
+                playerController.ConnectionID = player.ConnectionID;
+                playerController.PlayerIdNumber = player.PlayerIdNumber;
+                playerController.PlayerSteamId = player.PlayerSteamId;
+                playerController.PlayerName = player.PlayerName;
+            }        
+        }
+    }
+
+    
 }
