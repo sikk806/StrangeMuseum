500cd5000254bf6a15b532a1075dcc21a4b03817
sikk806 <sikk806@gmail.com>
2025-07-01 21:03:22 +0900

    Setting very Basic

---
+++ Assets/_StrangeMuseum/kjs/Scripts/Network/GameResultManager.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using Mirror;
+using System.Collections;
+
+public class GameResultManager : NetworkBehaviour
+{
+    public static GameResultManager Instance;
+
+    public int SecurityCount = 0;
+    public int StatueCount = 0;
+
+    private bool gameEnd = false;
+
+    public void SetCharacterCount(int securityNum, int statueNum)
+    {
+        SecurityCount += securityNum;
+        StatueCount += statueNum;
+    }
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isServer) return;
+        if (SecurityCount == 0) gameEnd = true;
+        if (gameEnd == true && StatueCount > 1)
+        {
+            SecurityCount = -1;
+            gameEnd = false;
+            StartCoroutine("GameResult");
+        }
+        Debug.Log("SecurityCount : " + SecurityCount + " StatueCount : " + StatueCount);
+    }
+
+    IEnumerator GameResult()
+    {
+        Debug.Log("Game Finish");
+        yield return null;
+    }
+
+}
